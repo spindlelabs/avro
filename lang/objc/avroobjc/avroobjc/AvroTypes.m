aa135d7ad6019106b3fec06ec9ce165dd33b456d
Keh-Li Sheng <kehli@bifflabs.com>
2012-01-27 18:40:20 -0500

    IPHONE-210 Avro CPP depends on Boost libraries. Including a project that builds Boost into a fake framework for easier inclusion into Xcode projects

---
+++ lang/objc/avroobjc/avroobjc/AvroTypes.m
@@ -0,0 +1,13 @@
+//
+//  AvroTypes.m
+//  avroobjc
+//
+//  Created by Keh-Li Sheng on 1/27/12.
+//  Copyright (c) 2012 Biff Labs. All rights reserved.
+//
+
+#import "AvroTypes.h"
+
+@implementation AvroTypes
+
+@end

00cbc5e35bb21e7672c44371220729cdf03adb8c
Keh-Li Sheng <kehli@bifflabs.com>
2012-01-29 23:25:06 -0500

    IPHONE-210 Xcode project for building the C++ Avro project for IOS.
    Xcode requires .ypp and .lpp suffixes for yacc and flex in order to support C++ code.
    The default C++ build calls bison with a custom header name "AvroYacc.hh" instead of the default "y.tab.h"

---
+++ lang/objc/avro/avroTests/avroTests.h
@@ -0,0 +1,13 @@
+//
+//  avroTests.h
+//  avroTests
+//
+//  Created by Keh-Li Sheng on 1/28/12.
+//  Copyright (c) 2012 Biff Labs. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface avroTests : SenTestCase
+
+@end
---
+++ lang/objc/avro/avroTests/avroTests.m
@@ -0,0 +1,32 @@
+//
+//  avroTests.m
+//  avroTests
+//
+//  Created by Keh-Li Sheng on 1/28/12.
+//  Copyright (c) 2012 Biff Labs. All rights reserved.
+//
+
+#import "avroTests.h"
+
+@implementation avroTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in avroTests");
+}
+
+@end
